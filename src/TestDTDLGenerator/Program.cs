93c46c6374679aeb2b9d5c62ced78443cf54bc20
Marco Parenzan <marco.parenzan@1nn0va.it>
2021-09-29 00:31:10 +0200

    NotComplete DTDL

---
+++ src/TestDTDLGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.DigitalTwins.Parser;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using System.Text.Json;
+
+var parser = new ModelParser();
+var text = File.ReadAllText("Solar Panel v1.1.json");
+var jsonDoc = JsonDocument.Parse(text);
+var xx = jsonDoc.RootElement.EnumerateArray().Select(xx => xx.ToString()).ToArray();
+var entities = await parser.ParseAsync(xx);
+
+var name = entities.Values.First().DisplayName;
+
+var itemClassDecl = SyntaxFactory.ClassDeclaration($"{name}")
+    .WithModifiers(SyntaxFactory.TokenList()
+        .Add(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+    )
+    .AddMembers(
+        entities.Skip(1).Select(xx =>
+            SyntaxFactory.PropertyDeclaration(SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.StringKeyword)), xx.Value.Id.ToString())
+            .WithModifiers(SyntaxFactory.TokenList().Add(SyntaxFactory.Token(SyntaxKind.PublicKeyword)))
+            .WithAccessorList(SyntaxFactory.AccessorList().AddAccessors(
+            SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration).WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)),
+            SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration).WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken))
+        ))
+        ).ToArray()
+    )
+;
+
+Console.WriteLine(itemClassDecl.NormalizeWhitespace());
+
+Console.ReadLine();
